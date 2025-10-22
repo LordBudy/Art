e5eab4b0bdd98f175339ee67412a8c65897c1f7e
budy <vesna1234510@gmail.com>
2025-10-22 16:34:50 +0300

    подрисовал кнопку в избранное

--- app/src/main/java/com/example/artphotoframe/core/presentation/ui/FavoritesButton.kt
+++ app/src/main/java/com/example/artphotoframe/core/presentation/ui/FavoritesButton.kt
@@ -1,12 +1,26 @@
 package com.example.artphotoframe.core.presentation.ui
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomEnd
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import com.example.artphotoframe.core.presentation.ui.theme.ArtPhotoFrameTheme
 
 
 @Composable
@@ -15,11 +29,46 @@ fun FavoritesButton(
     color: Color = MaterialTheme.colorScheme.onBackground,
     modifier: Modifier
 ) {
-    Text(
-        text = "⭐ Избранное",
-        color = color,
-        fontWeight = FontWeight.Bold,
-        modifier = modifier.clickable(onClick = onClick)
+    Box(
+        contentAlignment = BottomEnd ,
+        modifier = Modifier.fillMaxSize()
+            .padding(15.dp)
+    ) {
+        Box(
+            contentAlignment = Center,
+            modifier = Modifier
+                .width(140.dp).height(40.dp)
+                .border(width = 2.dp, color = Color.Green,
+                    shape = RoundedCornerShape(20.dp))
 
-    )
+        ){
+            Text(
+                text = "⭐ Избранное",
+                color = color,
+                fontWeight = FontWeight.Bold,
+                modifier = modifier
+                    .clickable(onClick = onClick)
+
+            )
+        }
+
+    }
+}
+@PreviewLightDark
+@Composable
+fun PreviewFavoritesButton() {
+
+    ArtPhotoFrameTheme {
+        Box(
+
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = MaterialTheme.colorScheme.background)
+        ) {
+            FavoritesButton(onClick = {},
+                color = MaterialTheme.colorScheme.onBackground,
+                modifier = Modifier
+            )
+        }
+    }
 }
